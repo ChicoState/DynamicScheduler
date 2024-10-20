7a31799f78c2c7bdf21ace53c35e0cc72bbe8f61
Leo Malinen <107366630+leo-malinen@users.noreply.github.com>
2024-10-19 19:32:43 -0700

    Update app.py with mongodb skeleton made

--- app/app.py
+++ app/app.py
@@ -3,53 +3,66 @@ from pymongo import MongoClient
 import os
 
 app = Flask(__name__)
-
 MONGO_URI = os.getenv('MONGO_URI')
 client = MongoClient(MONGO_URI)
 db = client.mydatabase
 
+# Initialize collections
+tasks_collection = db.tasks
+
+# Ensure indexes for better performance
+tasks_collection.create_index("day_number")
+
 # a web page for viewing a month
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
     # TODO: put in the correct month and day offset/num/last month, etc
-    return render_template('calendar.html', 
-                           action_name='dayView', first_day_offset=2, num_days=31, 
+    return render_template('calendar.html',
+                           action_name='dayView', first_day_offset=2, num_days=31,
                            month_name='October', last_month_days=30)
 
 # a web page for viewing the tasks for the day
-@app.route('/dayView', methods=['POST','GET'])
+@app.route('/dayView', methods=['POST', 'GET'])
 def day_view():
-    # TODO: Query what day number and day of the week we chose
-    # TODO: Consider if using request.view_args is better (aka .../dayView/13) (now,  .../dayView?dayNum=13)
-    #       Or maybe storing it locally is better?
-    # TODO: Query all tasks for this day from the database
-    # request.view_args['dayNum']
-    return render_template('dayView.html', day_number=int(request.args['dayNum']), day_name='Logsday', month_name='October',
-                           military_time=False,
-                           tasks=[
-                               {"start_time": 120, "duration": 60, "title": "from 2 to 3", "id": "event1", "is_task": False},
-                               {"start_time": 180, "duration": 30, "title": "from 3 to 3:30", "id": "task1", "is_task": True},
-                               {"start_time": 1200, "duration": 240, "title": "from 20 to 24", "id": "event2", "is_task": False}
-                            ])
+    day_number = int(request.args.get('dayNum', 1))
+    # Query all tasks for this day from the database
+    tasks = list(tasks_collection.find({"day_number": day_number}))
+    return render_template('dayView.html', day_number=day_number, day_name='Logsday', month_name='October', military_time=False, tasks=tasks)
 
 # a web page for adding a task
-@app.route('/addTask', methods=['POST','GET'])
+@app.route('/addTask', methods=['POST', 'GET'])
 def add_event():
     return render_template('addTask.html', day_number=int(request.args['dayNum']), day_name='Logsday', month_name='October')
 
-@app.route('/addTask/newTask', methods=['POST','GET'])
+@app.route('/addTask/newTask', methods=['POST', 'GET'])
 def receive_task():
     # request.form["task_name"]/request.form["from_time"]/request.form["to_time"]
     # can be used to get the name/from_time/to_time of the new event
-    # TODO: ADD A NEW TASK TO THE DATABASE
+    task_name = request.form["task_name"]
+    from_time = int(request.form["from_time"])
+    to_time = int(request.form["to_time"])
+    day_number = int(request.args['dayNum'])
+    
+    # ADD A NEW TASK TO THE DATABASE
+    task = {
+        "title": task_name,
+        "start_time": from_time,
+        "duration": to_time - from_time,
+        "day_number": day_number,
+        "is_task": True
+    }
+    tasks_collection.insert_one(task)
+    
     return redirect(url_for('index'))
 
-@app.route('/viewTask', methods=['POST','GET'])
+# a web page for viewing a task
+@app.route('/viewTask', methods=['POST', 'GET'])
 def view_task():
     # request.args['taskId'] to get the id of the task being displayed
-    # TODO: Make this page: it's empty rn
+    task_id = request.args['taskId']
+    task = tasks_collection.find_one({"_id": task_id})
     # ARGS TO RENDER_TEMPLATE ARE NOT FINAL
-    return render_template('viewTask.html', task_id=request.args['taskId'])
+    return render_template('viewTask.html', task=task)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
