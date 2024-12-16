1dba40aef01c0790aad5c9c5eacfffbaa7c08500
IvanGav <ivan.gavby@gmail.com>
2024-12-15 17:12:01 -0800

    improvements in addtask page and adding tasks works again

--- app/app.py
+++ app/app.py
@@ -54,26 +54,64 @@ def add_event():
 
 @app.route(pathCreateEvent, methods=['POST', 'GET'])
 def receive_event():
+    day_number=int(request.args['dayNum']) # is redundant, since we have 'start_date', but somebody didn't write a python util function to parse date into day, month, year
+
     name = request.form["event_name"]
     description = request.form["event_description"]
-    from_time = request.form["from_time"]
-    to_time = request.form["to_time"]
-    day_number = int(request.args['dayNum'])
+    start_date = request.form["start_date"]
+    is_task = request.form["is_task"] == "true"
+    if is_task:
+        due_date = request.form["due_date"]
+        duration = int(request.form["duration"])
+        try:
+            _ = request.form["can_split"]
+            can_split = True
+        except:
+            can_split = False
+    else:
+        from_time = request.form["from_time"]
+        to_time = request.form["to_time"]
     
-    start_time = util.time_to_minutes(from_time)
-    end_time = util.time_to_minutes(to_time)
-    duration = end_time - start_time
+        start_time = util.time_to_minutes(from_time)
+        end_time = util.time_to_minutes(to_time)
+        duration = end_time - start_time
 
-    task = {
-        "title": name,
-        "description": description,
-        "from_time": from_time,
-        "to_time": to_time,
-        "start_time_mfm": start_time,
-        "duration_minutes": duration,
-        "day_number": day_number,
-        "is_task": False
-    }
+    # testing the inputs vvv
+    # if is_task:
+    #     return render_template('debug.html', 
+    #                        display=f"task: name={name}; desc={description}; sdate={start_date}; task={is_task}; can_split={can_split}; duration={duration}")
+    # else:
+    #     return render_template('debug.html', 
+    #                        display=f"event: name={name}; desc={description}; sdate={start_date}; task={is_task}; from_time={from_time}; duration={duration}")
+    
+    if is_task:
+        # is a task
+        task = {
+            "title": name,
+            "description": description,
+            "start_date": start_date,
+            "from_time": from_time,
+            "day_number": day_number,
+            "is_task": True,
+            "due_date": due_date,
+            "duration_minutes": duration,
+            "can_split": can_split,
+        }
+    else:
+        # an event
+        task = {
+            "title": name,
+            "description": description,
+            "start_date": start_date,
+            "from_time": from_time,
+            "day_number": day_number,
+            "is_task": True,
+            "from_time": from_time, # redundant, but used
+            "to_time": to_time, # redundant, but used
+            "start_time_mfm": start_time,
+            "duration_minutes": duration,
+        }
+
     db.add_task(task)
     
     return redirect(util.formatURI(pathViewDay, dayNum=day_number))
